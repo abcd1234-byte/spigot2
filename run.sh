9063d1d4aef89fac431921698bd00446a14d3cef
abcd1234-byte <25rogers.jacob@rrcs.org>
2021-12-08 01:25:08 +0000

    first commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+sdk install java
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+java -jar BuildTools.jar --rev 1.18 --remapped
+java -jar BuildTools.jar --rev 1.18 --remapped --compile craftbukkit
